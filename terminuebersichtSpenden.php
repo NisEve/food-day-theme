d733457e1fa450f197722e674ad95995f206ede3
Nis Evers <nis.evers@rtl.de>
2026-02-22 17:36:07 +0100

    add list

---
+++ terminuebersichtSpenden.php
@@ -0,0 +1,47 @@
+<@ elements/header.php @>
+<body>
+
+<@ elements/navbar.php @>
+
+<main class="dateOverview main">
+
+	<section class="pageIntroSection plainTextIntroSection">
+
+		<div class="pageIntroDates pageIntroPlaintText">
+			<div>
+				<h1>@{ TerminHeadline | def ('Termine') } </h1>
+
+				<p>@{ IntroText | def ('Einleitender Info Text ') }
+				</p>
+
+				<button
+					type="button"
+					class="subscribeCalendar"
+					data-calendar-source="pickup"
+					data-calendar-title="Food Day Abholtermine">
+					Termine herunterladen und im Kalender speichern
+				</button>
+
+			</div>
+		</div>
+
+	</section>
+	<div class="wave introWave" aria-hidden="true"></div>
+
+	<section class="pageSection pageDatesSection">
+
+		@{ +BlocksTerminInhalt }
+
+
+	</section>
+
+	<section>
+	</section>
+
+</main>
+
+
+</body>
+</html>
+
+<@ elements/footer.php @>
